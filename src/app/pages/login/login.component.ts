270ab5adec1612e31e0a1561a9ecd7acc2434840
A Venkatesh <avenkateshanand1998@gmail.com>
2024-07-01 23:43:24 +0530

    I Have To Develop the Login page and layout page here

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+ 
+    loginObj: any = {
+      userName:"",
+      password:""
+    }
+     
+    router = inject(Router)
 
+    onLogin(){
+      if(this.loginObj.userName == "venkat" && this.loginObj.password == "nandu"){
+        alert("User Login Successfully");
+        this.router.navigateByUrl("/dashboard")
+      }else{
+        alert("InValid UserId and Password");
+      }
+    }
 }
