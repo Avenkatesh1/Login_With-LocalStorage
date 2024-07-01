086d047890c9bb267f129424fead476acbfe47a5
A Venkatesh <avenkateshanand1998@gmail.com>
2024-07-01 23:14:01 +0530

    imported the bootstrap in my locally

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
